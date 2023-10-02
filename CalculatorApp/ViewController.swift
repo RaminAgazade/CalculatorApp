4b84da60a9fb0ccb0462ea38121266ec95e5bf04
RaminAgazade <130837899+RaminAgazade@users.noreply.github.com>
2023-10-03 00:47:04 +0400

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Ramin on 03.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
